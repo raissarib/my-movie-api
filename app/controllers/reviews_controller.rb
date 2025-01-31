551ca5bf5a046a7fd38a61debc4fe42ae484aba3
raissarib <raissa.rib@edu.unifor.br>
2025-01-31 19:58:47 -0300

    feat: add reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,24 @@
 class ReviewsController < ApplicationController
+  def index
+    reviews = Review.all
+    render json: reviews
+  end
+
+  def show
+    review = Review.find(params[:id])
+    render json: review
+  end
+
+  def create
+    review = Review.new(review_params)
+    if review.save
+      render json: review, status: :created
+    else
+      render json: { errors: review.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:user_id, :movie_id, :rating, :comment)
+  end
 end
