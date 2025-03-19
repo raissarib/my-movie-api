dffe7383389553bcf02cc4022aa5ef794ffc5ba6
raissarib <raissa.rib@edu.unifor.br>
2025-03-19 18:56:24 -0300

    fix: remove validates from movie table

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,13 +1,3 @@
 class Movie < ApplicationRecord
   has_many :reviews, dependent: :destroy
-
-  enum genre: {
-    action: "Action",
-    drama: "Drama",
-    comedy: "Comedy",
-    sci_fi: "Sci-Fi",
-    thriller: "Thriller"
-  }
-
-  validates :genre, inclusion: { in: genres.keys }
 end
