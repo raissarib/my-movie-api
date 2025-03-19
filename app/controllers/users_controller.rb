72821df5d35f270777b118ee0f051b190c2e1e0f
raissarib <raissa.rib@edu.unifor.br>
2025-03-19 18:57:18 -0300

    feat: improve users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [ :show ]
+
   def index
     users = User.all
     render json: users
   end
 
   def show
-    user = User.find(params[:id])
-    render json: user
+    render json: @user
   end
 
   def create
@@ -16,6 +17,27 @@ class UsersController < ApplicationController
     else
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Usuário não encontrado" }, status: :not_found
   end
 
   def user_params
