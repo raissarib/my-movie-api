eb470a74f6590978013aaadefe1c5ea31260540e
raissarib <raissa.rib@edu.unifor.br>
2025-03-19 18:56:43 -0300

    feat: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+user = User.create(name: "Bob", email: "bob@example.com")
+movie = Movie.create(title: "Inception", genre: "Science Fiction", year: 2010, director: "Christopher Nolan")
+Review.create(user: user, movie: movie, rating: 8.7, comment: "Very interesting movie with a complex plot!")
