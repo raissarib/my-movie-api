03a49c7600e0741ca7532af51bb0d044b12270b4
raissarib <raissa.rib@edu.unifor.br>
2025-03-19 18:11:09 -0300

    feat: add associations and validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  enum genre: {
+    action: "Action",
+    drama: "Drama",
+    comedy: "Comedy",
+    sci_fi: "Sci-Fi",
+    thriller: "Thriller"
+  }
+
+  validates :genre, inclusion: { in: genres.keys }
 end
